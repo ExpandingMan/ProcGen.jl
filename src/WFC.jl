807fa97b8841cb9ce4773c06b5340be4613ee281
ExpandingMan <savastio@protonmail.com>
2020-02-28 16:03:56 -0500

    added output

--- src/WFC.jl
+++ src/WFC.jl
@@ -5,16 +5,36 @@ struct Input{T,R,A<:AbstractArray} <: AbstractArray{T,R}
     n::NTuple{R,Int}
 end
 
-axislength(η::Input, a::Integer) = max(1, size(η.x, a) - η.n[a] + 1)
+axislength(ζ::Input, a::Integer) = max(1, size(ζ.x, a) - ζ.n[a] + 1)
 
-Base.size(η::Input) = ntuple(n -> axislength(η, n), ndims(η))
+Base.size(ζ::Input) = ntuple(n -> axislength(ζ, n), ndims(ζ))
 
-indexrange(η::Input, a::Integer, i::Integer) = i:(i + η.n[a] - 1)
+indexrange(ζ::Input, a::Integer, i::Integer) = i:(i + ζ.n[a] - 1)
 
 # TODO have to get rid of splatting at least in low rank cases
-function Base.getindex(η::Input, I::Vararg{Int, N}) where {N}
-    getindex(η.x, (indexrange(η, a, i) for (a, i) ∈ enumerate(I))...)
+function Base.getindex(ζ::Input, I::Vararg{Int, N}) where {N}
+    getindex(ζ.x, (indexrange(ζ, a, i) for (a, i) ∈ enumerate(I))...)
 end
+
+
+struct Output{T,Ro,A<:AbstractArray,I<:Input} <: AbstractArray{T,Ro}
+    ζ::I
+    y::A  # first index will be new index
+end
+
+function Output(n::Tuple, ζ::Input, ::Type{T}=Bool) where {T}
+    dims = tuple(size(ζ)..., n...)
+    y = ones(T, dims)
+    Output{T,length(n),typeof(y),typeof(ζ)}(ζ, y)
+end
+
+Base.size(η::Output) = ntuple(i -> size(η.y, i+ndims(η.ζ)), ndims(η))
+
+# TODO again, probably ridiculously inefficient, but for now...
+function Base.getindex(η::Output, I::Vararg{Int,N}) where {N}
+    getindex(η.y, ntuple(i -> Colon(), ndims(η.ζ))..., I...)
+end
+Base.IndexStyle(::Output) = IndexCartesian()
 
 
 end
--- test/scrap.jl
+++ test/scrap.jl
@@ -1,7 +1,11 @@
 using ProcGen: WFC
+using Random, BenchmarkTools
 
-using ProcGen.WFC: Input
+using ProcGen.WFC: Input, Output
+
+Random.seed!(999)
 
 x = rand(0:15, 5, 6)
 
-η = Input{Matrix{Int},2,typeof(x)}(x, (2,3))
+ζ = Input{Matrix{Int},2,typeof(x)}(x, (2,3))
+η = Output((8,10), ζ)
